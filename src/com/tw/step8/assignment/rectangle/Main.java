2b17ad5916745d9da84789dce24eb15bce2a9386
chhavid <99489321+chhavid@users.noreply.github.com>
2022-09-14 11:27:11 +0530

    Changed package name

--- src/com/tw/step8/assignment/rectangle/Main.java
+++ src/com/tw/step8/assignment/rectangle/Main.java
@@ -1,4 +1,4 @@
-package com.tw.step8.rectangle;
+package com.tw.step8.assignment.rectangle;
 
 
 public class Main {
--- test/com/tw/step8/assignment/rectangle/RectangleTest.java
+++ test/com/tw/step8/assignment/rectangle/RectangleTest.java
@@ -1,15 +1,21 @@
-package com.tw.step8.rectangle;
+package com.tw.step8.assignment.rectangle;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RectangleTest { 
+class RectangleTest {
 
   @Test
   void shouldCalculateArea() {
     Rectangle rectangle = new Rectangle(2, 2);
     assertEquals(rectangle.area(),4);
+  }
+
+  @Test
+  void shouldCalculateAreaOfRectangleWithSidesInPoints() {
+    Rectangle rectangle = new Rectangle(3.5, 2);
+    assertEquals(rectangle.area(),7);
   }
 
   @Test
