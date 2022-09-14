5c7f1cceba7bacea84a0a0f9abbcd4b1cd672c56
chhavid <99489321+chhavid@users.noreply.github.com>
2022-09-14 11:18:57 +0530

    Added method in Rectangle to calcualte perimeter

--- src/com/tw/step8/rectangle/Rectangle.java
+++ src/com/tw/step8/rectangle/Rectangle.java
@@ -13,4 +13,8 @@ public class Rectangle {
   public int area() {
     return lenght*breadth;
   }
+
+  public int perimeter() {
+    return 2*(lenght + breadth);
+  }
 }
--- test/com/tw/step8/rectangle/RectangleTest.java
+++ test/com/tw/step8/rectangle/RectangleTest.java
@@ -4,11 +4,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RectangleTest {
+class RectangleTest { 
 
   @Test
   void shouldCalculateArea() {
     Rectangle rectangle = new Rectangle(2, 2);
     assertEquals(rectangle.area(),4);
+  }
+
+  @Test
+  void shouldCalculatePerimeter() {
+    Rectangle rectangle = new Rectangle(2, 2);
+    assertEquals(rectangle.perimeter(),8);
   }
 }
