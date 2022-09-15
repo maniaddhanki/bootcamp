422757772fbd2d328405bedb471c0d1bd42b7a30
Addhanki Manikanta <99521159+maniaddhanki@users.noreply.github.com>
2022-09-15 13:57:04 +0530

    Added validation for length

--- src/com/tw/step8/assignment3/Length.java
+++ src/com/tw/step8/assignment3/Length.java
@@ -1,10 +1,12 @@
 package com.tw.step8.assignment3;
 
+import com.tw.step8.assignment3.exception.InvalidLengthException;
+
 public class Length {
     private final double value;
     private final Unit unit;
 
-    public Length(double value, Unit unit) {
+    private Length(double value, Unit unit) {
         this.value = value;
         this.unit = unit;
     }
@@ -21,5 +23,12 @@ public class Length {
             return Relation.EQUAL;
          }
         return lengthInInch > givenLengthInInch ? Relation.GREATER: Relation.LESSER;
+    }
+
+    public static Length createLength(double value, Unit unit) throws InvalidLengthException {
+        if (value < 0){
+            throw new InvalidLengthException(value);
+        }
+        return new Length(value, unit);
     }
 }
--- test/com/tw/step8/assignment3/LengthTest.java
+++ test/com/tw/step8/assignment3/LengthTest.java
@@ -1,56 +1,62 @@
 package com.tw.step8.assignment3;
 
+import com.tw.step8.assignment3.exception.InvalidLengthException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LengthTest {
     @Test
-    void shouldRelateSameLengthsAsEqual() {
-        Length length1 = new Length(4, Unit.INCH);
-        Length length2 = new Length(4, Unit.INCH);
+    void shouldThrowAnExceptionForNegativeLength() {
+        assertThrows(InvalidLengthException.class,()->Length.createLength(-1,Unit.INCH));
+    }
+
+    @Test
+    void shouldRelateSameLengthsAsEqual() throws InvalidLengthException {
+        Length length1 = Length.createLength(4, Unit.INCH);
+        Length length2 = Length.createLength(4, Unit.INCH);
         assertEquals(Relation.EQUAL,length1.compare(length2));
     }
 
     @Test
-    void shouldRelateGreaterIfGivenLengthIsSmaller() {
-        Length length1 = new Length(4, Unit.INCH);
-        Length length2 = new Length(2, Unit.INCH);
+    void shouldRelateGreaterIfGivenLengthIsSmaller() throws InvalidLengthException {
+        Length length1 = Length.createLength(4, Unit.INCH);
+        Length length2 = Length.createLength(2, Unit.INCH);
         assertEquals(Relation.GREATER,length1.compare(length2));
     }
 
     @Test
-    void shouldRelateLesserIfGivenLengthIsLarger() {
-        Length length1 = new Length(4, Unit.INCH);
-        Length length2 = new Length(8, Unit.INCH);
+    void shouldRelateLesserIfGivenLengthIsLarger() throws InvalidLengthException {
+        Length length1 = Length.createLength(4, Unit.INCH);
+        Length length2 = Length.createLength(8, Unit.INCH);
         assertEquals(Relation.LESSER,length1.compare(length2));
     }
 
     @Test
-    void shouldCompareLengthsInFeetAndInch() {
-        Length feet = new Length(1, Unit.FEET);
-        Length inch = new Length(12, Unit.INCH);
+    void shouldCompareLengthsInFeetAndInch() throws InvalidLengthException {
+        Length feet = Length.createLength(1, Unit.FEET);
+        Length inch = Length.createLength(12, Unit.INCH);
         assertEquals(Relation.EQUAL,feet.compare(inch));
     }
 
     @Test
-    void shouldCompareLengthsInInchAndCentimeter() {
-        Length cm = new Length(5, Unit.CM);
-        Length inch = new Length(3, Unit.INCH);
+    void shouldCompareLengthsInInchAndCentimeter() throws InvalidLengthException {
+        Length cm = Length.createLength(5, Unit.CM);
+        Length inch = Length.createLength(3, Unit.INCH);
         assertEquals(Relation.LESSER,cm.compare(inch));
     }
 
     @Test
-    void shouldCompareLengthsInMillimeterAndCentimeter() {
-        Length cm = new Length(1, Unit.CM);
-        Length mm = new Length(10, Unit.MM);
+    void shouldCompareLengthsInMillimeterAndCentimeter() throws InvalidLengthException {
+        Length cm = Length.createLength(1, Unit.CM);
+        Length mm = Length.createLength(10, Unit.MM);
         assertEquals(Relation.EQUAL,cm.compare(mm));
     }
 
     @Test
-    void shouldCompareLengthsInMillimeterAndInch() {
-        Length mm = new Length(100, Unit.MM);
-        Length inch = new Length(4, Unit.INCH);
+    void shouldCompareLengthsInMillimeterAndInch() throws InvalidLengthException {
+        Length mm = Length.createLength(100, Unit.MM);
+        Length inch = Length.createLength(4, Unit.INCH);
         assertEquals(Relation.EQUAL,inch.compare(mm));
     }
 }
